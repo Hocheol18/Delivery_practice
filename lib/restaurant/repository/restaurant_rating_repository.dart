0268f34be2c7defe35a69004db0457656b747333
Hocheol <didifia9850@naver.com>
2025-05-12 13:59:35 +0900

    2025.05.12

---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,20 @@
+import 'package:delivery/rating/model/rating_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+
+import '../../common/model/cursor_pagination_model.dart';
+import '../../common/model/pagination_params.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+@RestApi()
+abstract class RestaurantRatingRepository{
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) = _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({'accessToken': 'true'})
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+
+}
